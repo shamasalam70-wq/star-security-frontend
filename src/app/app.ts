2e707ac696f634e2bb567bb06c7b94116ba99f78
Muhammad Shamas <shamasalam70@gmail.com>
2026-02-07 19:44:50 +0500

    dccc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/navbar/navbar';
-import { Footer } from './shared/footer/footer'; 
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar,Footer],  
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Eproject-UI');
+
+  constructor(public router: Router) {}
+
+  isAdminRoute() {
+    return this.router.url.startsWith('/admin');
+  }
 }
